25b8c23eabe3451be9a98352cf055574797e7d99
nachal <nachalsa@naver.com>
2025-06-29 17:19:34 +0900

    feat: main 생성

---
+++ main.py
@@ -0,0 +1,20 @@
+# 사용 예제 파일: main.py
+"""
+OpenVINO Pose Estimation 사용 예제
+
+이 파일은 dark_templar패키지의 사용법을 보여줍니다.
+"""
+
+from dark_templar.xdotool_usage import run_proximity_trigger_example
+
+
+
+if __name__ == "__main__":
+    
+    # 예제 실행
+    print("OpenVINO Dark Templar - 웹캠 근접 감지 트리거를 시작합니다.\n")
+    
+    # 확장 기능 예제를 직접 실행
+    run_proximity_trigger_example()
+
+    print("\n Adun Toridas ")
